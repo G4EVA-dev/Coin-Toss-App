66992b3dd8414d111cbea04c905c185a1c9c8d4e
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-26 23:41:12 +0100

    add coin flip functionality

--- views/gameView/components/flipBtn.js
+++ views/gameView/components/flipBtn.js
@@ -1,20 +1,44 @@
 import gameDataStore from "../../../stores/gameDataStore";
-import sounds from "../../../stores/soundStore"
+import sounds from "../../../stores/soundStore";
 import documentElementStore from "../../../stores/documentElementStore";
 
+export const flipCoin = function () {
+  sounds.flip.play();
+  let machineGuess = gameDataStore.setRandomMachineGuess();
+  if (machineGuess) {
+    setTimeout(() => {
+      documentElementStore.coin.style.animation = "spin-head 3s forwards";
+      console.log("headSpins");
+    }, 100);
+  } else {
+    setTimeout(() => {
+      documentElementStore.coin.style.animation = "spin-tail 3s forwards";
+    }, 100);
+  }
+};
 
-export const flipBtnAction = function (){
-    sounds.flip.play();
-    let machineGuess = gameDataStore.setRandomMachineGuess();
-    if(machineGuess){
-        setTimeout(()=>{
-            documentElementStore.coin.style.animation = "spin-head 3s forwards";
-            console.log("headSpins") 
-        },100)
-    }else{
-        setTimeout(()=>{
-            documentElementStore.coin.style.animation = "spin-tail 3s forwards";   
-        },100)
-    }
-}
+documentElementStore.coin.addEventListener("click", flipCoin);
+
+
+
+
+// import gameDataStore from "../../../stores/gameDataStore";
+// import sounds from "../../../stores/soundStore"
+// import documentElementStore from "../../../stores/documentElementStore";
+
+
+// export const flipBtnAction = function (){
+//     sounds.flip.play();
+//     let machineGuess = gameDataStore.setRandomMachineGuess();
+//     if(machineGuess){
+//         setTimeout(()=>{
+//             documentElementStore.coin.style.animation = "spin-head 3s forwards";
+//             console.log("headSpins") 
+//         },100)
+//     }else{
+//         setTimeout(()=>{
+//             documentElementStore.coin.style.animation = "spin-tail 3s forwards";   
+//         },100)
+//     }
+// }
 
