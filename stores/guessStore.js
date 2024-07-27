994ccb87faa6df29239fbc175bdf8a440d146a48
G4EVA-dev <tanzeglenn@gmail.com>
2024-07-27 02:12:43 +0100

    coin is flipping

--- stores/guessStore.js
+++ stores/guessStore.js
@@ -2,9 +2,9 @@ export const playerGuess = {
     guess : null,
     updateGuess(){
         if(headGuess.isChoosen){
-            this.guess = 1;
-        }else{
             this.guess = 0;
+        }else{
+            this.guess = 1;
         }
     }
 }
--- views/gameView/components/flipBtn.js
+++ views/gameView/components/flipBtn.js
@@ -5,6 +5,13 @@ import documentElementStore from "../../../stores/documentElementStore";
 export const flipCoin = function () {
   sounds.flip.play();
   let machineGuess = gameDataStore.setRandomMachineGuess();
+  console.log(machineGuess);
+
+  // Clear any existing animations
+  documentElementStore.coin.style.animation = "none";
+  // Force reflow to reset animation
+  documentElementStore.coin.offsetHeight;
+
   if (machineGuess) {
     setTimeout(() => {
       documentElementStore.coin.style.animation = "spin-head 3s forwards";
@@ -13,11 +20,39 @@ export const flipCoin = function () {
   } else {
     setTimeout(() => {
       documentElementStore.coin.style.animation = "spin-tail 3s forwards";
+      console.log("tailSpins");
     }, 100);
   }
 };
 
+
 documentElementStore.coin.addEventListener("click", flipCoin);
+
+
+
+
+
+// import gameDataStore from "../../../stores/gameDataStore";
+// import sounds from "../../../stores/soundStore";
+// import documentElementStore from "../../../stores/documentElementStore";
+
+// export const flipCoin = function () {
+//   sounds.flip.play();
+//   let machineGuess = gameDataStore.setRandomMachineGuess();
+//   if (machineGuess) {
+//     setTimeout(() => {
+//       documentElementStore.coin.style.animation = "spin-head 3s forwards";
+//       console.log("headSpins");
+//     }, 100);
+//   } else {
+//     setTimeout(() => {
+//       documentElementStore.coin.style.animation = "spin-tail 3s forwards";
+//       console.log("TailSpins");
+//     }, 100);
+//   }
+// };
+
+// documentElementStore.coin.addEventListener("click", flipCoin);
 
 
 
